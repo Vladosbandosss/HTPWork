c6696fd224ff7ea29ab8fe1d02bf8b6e125f340c
Vladosbandosss <dasistperfektos@gmail.com>
2021-03-08 11:52:50 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
-using System.Text;
-namespace Test
+
+namespace StringTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
-
+            MyString myString = new MyString("Hi");
+            MyString myString1 = new MyString("Stroka", 5);
+            myString.HowMachGlass("аба");
+            myString.IfContains("волк", "a");
+            myString.IfContains("волк", "о");
         }
     }
 }
